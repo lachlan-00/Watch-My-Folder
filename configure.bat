1fc9b865f42427f456746974dd0a6485462c0886
lachlan <lachlan.00@gmail.com>
2012-11-15 16:38:46 +1000

    added gnuwin32 unzip for extraction. updated install batch file.

--- configure.bat
+++ configure.bat
@@ -1,8 +1,11 @@
 @echo off
+set COPYCMD=/Y
 set PWD=%cd%
+echo Extracting Portable Python
+tools\unzip -o Python\python.zip -d Python\
 set newpath=;%PWD%\Python;%PWD%\DLLs;%PWD%\Scripts
 set PATH=%PATH%%newpath%
 start Python\python.exe watch\configure.py
 mkdir "%appdata%\Microsoft\Windows\Start Menu\Watch My Folder"
-xcopy /Y "Watch My Folder.lnk" "%appdata%\Microsoft\Windows\Start Menu\Watch My Folder\"
-xcopy /Y "Edit Config File.lnk" "%appdata%\Microsoft\Windows\Start Menu\Watch My Folder\"
+xcopy "Watch My Folder.lnk" "%appdata%\Microsoft\Windows\Start Menu\Watch My Folder\"
+xcopy "Edit Config File.lnk" "%appdata%\Microsoft\Windows\Start Menu\Watch My Folder\"
---
+++ tools/README.txt
@@ -0,0 +1,2 @@
+Taken from:
+http://gnuwin32.sourceforge.net/packages/unzip.htm
